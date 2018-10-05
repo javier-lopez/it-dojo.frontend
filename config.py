59879ad1955af13e580806546fec715d03d8aa77
Javier López <m@javier.io>
2018-10-04 21:00:36 -0500

    update mongodb connection defaults in config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ class Config(object):
     MONGODB_HOST     = os.environ.get('MONGODB_HOST')     or 'mongodb'
     MONGODB_TCP_PORT = os.environ.get('MONGODB_TCP_PORT') or 27017
     MONGODB_TCP_PORT = int(MONGODB_TCP_PORT)
-    MONGODB_DB       = os.environ.get('MONGODB_DB')       or 'itdojo'
-    MONGODB_USER     = os.environ.get('MONGODB_USER')     or 'itdojo'
-    MONGODB_PASSWD   = os.environ.get('MONGODB_PASSWD')   or 'itdojo'
+    MONGODB_DB       = os.environ.get('MONGODB_DB')       or 'app'
+    MONGODB_USER     = os.environ.get('MONGODB_USER')     or 'app'
+    MONGODB_PASSWD   = os.environ.get('MONGODB_PASSWD')   or 'app'
 
     MONGODB_USER     = urllib.parse.quote_plus(MONGODB_USER)
     MONGODB_PASSWD   = urllib.parse.quote_plus(MONGODB_PASSWD)
